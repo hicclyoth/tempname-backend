a8aafb3d8b7328e807804a24f72922da0e2b885d
hicclyoth <sumiplorem@gmail.com>
2024-10-16 02:04:29 +0800

    Added CORS

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
